93862e2ec09fdc734dc7a9a3f1ff846a3b8c2ce9
tonyJonas <tony.sousa@sou.inteli.edu.br>
2025-05-24 03:49:51 -0300

    UPDATE: updating files to deploy

--- frontend/src/config/constants.ts
+++ frontend/src/config/constants.ts
@@ -1,5 +1,7 @@
 // /home/ubuntu/aquachain_mvp/frontend/src/config/constants.ts
 
+// Antes
+
 export const PACKAGE_ID =
   "0x855a7ecdcc2a466ca24fa71e6cf12aaa2a172cfaa72aeb26c92b5f642c1d892d";
 export const ECOSYSTEM_STATE_ID =
@@ -8,7 +10,7 @@ export const TREASURY_CAP_ID =
   "0x9fbd7e54e4eeff730cf8c425f0da91b473742184e849a2b8892588cdbd74832a";
 
 // Mock API base URL (running locally)
-export const MOCK_API_URL = "http://127.0.0.1:5001/api";
+export const MOCK_API_URL = "https://tonyjonas.pythonanywhere.com/api";
 
 // Mock addresses (used for API calls when no real address is available or for specific mock data)
 export const MOCK_USER_ADDRESS_1 = "0xUSER1_MOCK_ADDRESS";
