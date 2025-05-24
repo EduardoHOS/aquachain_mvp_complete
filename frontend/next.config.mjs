606bed82de2b0929498915283e3242d6837e870d
tonyJonas <tony.sousa@sou.inteli.edu.br>
2025-05-24 03:55:43 -0300

    UPDATE: updating files to deploy

---
+++ frontend/next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    eslint: {
+      // Ignora todos os erros do ESLint durante `next build`
+      ignoreDuringBuilds: true,
+    },
+  };
+  
+  export default nextConfig;
+  
